b0c5d2d14abe5432685d8ba7816616a8393881ba
Yoni254 <tzukermany@gmail.com>
2021-08-31 01:42:15 +0300

    implemented dice roll command

--- core/commands/user/diceroll.js
+++ core/commands/user/diceroll.js
@@ -5,10 +5,65 @@ module.exports = {
 	// Defines the Basic Command Data
 	data: new SlashCommandBuilder()
 		.setName(prefix + 'roll')
-		.setDescription('Replies with User Info!'),
+		.setDescription('Replies with User Info!')
+		.addStringOption(option =>
+			option.setName('num')
+				.setDescription('Number of dice')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('type')
+				.setDescription('Type of dice')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('multiplier')
+				.setDescription('Multiply the dice roll')
+				.setRequired(false)),
 
 	// Is executed when the command is called.
 	async execute(interaction) {
-		await interaction.reply({ content: 'rolling' });
+
+		if (interaction.guild) {
+			console.log(`A user requested the roll command in ${interaction.guild.name}`);
+		} else {
+			console.log('A user requested the roll command in a private message');
+		}
+
+
+		// grab user options
+		const num = parseInt(interaction.options.get('num').value);
+		const type = parseInt(interaction.options.get('type').value) + 1;
+		const multiplier = (interaction.options.get('multiplier')) ? parseInt(interaction.options.get('multiplier').value) : 0;
+
+
+		// error handling
+		// Prepping Error messages in advance
+		const diceError = 'Did you type it wrong? Example: `>roll 1d10` or `>roll 1d10 +10`';
+		if (num > 100) return await interaction.reply({ content: 'You can only do below 100 dice!' });
+		if (num < 1) return await interaction.reply({ content: 'You can\'t roll dice that\'s below 1' });
+		if (type > 10000) return await interaction.reply({ content: 'You can only do below 10000 for the number of sides for the dice!' });
+		if (type < 2) return await interaction.reply({ content: 'You can\'t roll dice with sides that\'s below 2' });
+
+
+		// the actual calculation
+		try {
+			const result = [];
+			// generate 'num' number of random numbers
+			for (let i = 0; i < num; i++) {
+				// some formula I definitely didn't find in stack overflow
+				const rand = Math.floor(Math.random() * type) + 1;
+				result.push(rand);
+			}
+
+			// build the answer string
+			const randomResult = result.join(', ');
+			const totalNumber = result.reduce((a, b) => a + b, 0) + multiplier;
+			const finalAnswer = (multiplier !== 0) ?
+				`Rolled: ${randomResult}, +${multiplier}\nTotal: ${totalNumber}` :
+				`Rolled: ${randomResult}\nTotal: ${totalNumber}`;
+
+			await interaction.reply({ content: finalAnswer });
+		} catch {
+			await interaction.reply({ content: diceError });
+		}
 	},
 };
