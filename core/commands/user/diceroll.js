3480c0edac563b90da4be3357615b74efb6dc81a
Yoni254 <tzukermany@gmail.com>
2021-08-31 12:20:46 +0300

    back to new syntax for diceroll

--- core/commands/user/diceroll.js
+++ core/commands/user/diceroll.js
@@ -28,24 +28,25 @@ module.exports = {
 			console.log('A user requested the roll command in a private message');
 		}
 
-
-		// grab user options
-		const num = parseInt(interaction.options.get('num').value);
-		const type = parseInt(interaction.options.get('type').value) + 1;
-		const multiplier = (interaction.options.get('multiplier')) ? parseInt(interaction.options.get('multiplier').value) : 0;
-
-
-		// error handling
 		// Prepping Error messages in advance
 		const diceError = 'Did you type it wrong? Example: `>roll 1d10` or `>roll 1d10 +10`';
-		if (num > 100) return await interaction.reply({ content: 'You can only do below 100 dice!' });
-		if (num < 1) return await interaction.reply({ content: 'You can\'t roll dice that\'s below 1' });
-		if (type > 10000) return await interaction.reply({ content: 'You can only do below 10000 for the number of sides for the dice!' });
-		if (type < 2) return await interaction.reply({ content: 'You can\'t roll dice with sides that\'s below 2' });
 
 
-		// the actual calculation
 		try {
+
+			// grab user options
+			const num = parseInt(interaction.options.get('num').value);
+			const type = parseInt(interaction.options.get('type').value) + 1;
+			const multiplier = (interaction.options.get('multiplier')) ? parseInt(interaction.options.get('multiplier').value) : 0;
+
+
+			// error handling
+			if (num > 100) return await interaction.reply({ content: 'You can only do below 100 dice!' });
+			if (num < 1) return await interaction.reply({ content: 'You can\'t roll dice that\'s below 1' });
+			if (type > 10000) return await interaction.reply({ content: 'You can only do below 10000 for the number of sides for the dice!' });
+			if (type < 2) return await interaction.reply({ content: 'You can\'t roll dice with sides that\'s below 2' });
+
+			// the actual calculation
 			const result = [];
 			// generate 'num' number of random numbers
 			for (let i = 0; i < num; i++) {
