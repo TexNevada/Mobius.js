b6a3583bc7ba0ec7699d71da4d7e2fb91fe5f343
TexNevada <15308761+texnevada@users.noreply.github.com>
2021-12-13 23:51:25 +0100

    Added some info

--- App/dependencies/commands/serverinfo.js
+++ App/dependencies/commands/serverinfo.js
@@ -7,8 +7,18 @@ module.exports = {
 		.setDescription('Displays server information'),
 	async execute(interaction) {
 		const GuildOwner = await interaction.guild.fetchOwner();
-		const Channels = interaction.guild.channels.GuildChannelManager.channelCountWithoutThreads;
-		const Threads = await interaction.guild.channels.GuildChannelManager.fetchActiveThreads();
+		// Channels
+		const Channels = interaction.guild.channels.cache.filter(c => c.type === 'GUILD_TEXT').size;
+		const Public_Threads = interaction.guild.channels.cache.filter(c => c.type === 'GUILD_PUBLIC_THREAD').size;
+		const Private_Threads = interaction.guild.channels.cache.filter(c => c.type === 'GUILD_PRIVATE_THREAD').size;
+		const News_Threads = interaction.guild.channels.cache.filter(c => c.type === 'GUILD_NEWS_THREAD').size;
+		const Voice = interaction.guild.channels.cache.filter(c => c.type === 'GUILD_VOICE').size;
+		// Users & Bots
+		const Users = interaction.guild.members.cache.filter(member => !member.user.bot).size;
+		const Bots = interaction.guild.members.cache.filter(member => member.user.bot).size;
+		const Members = interaction.guild.memberCount;
+
+		// const Threads = await interaction.guild.channels.GuildChannelManager.fetchActiveThreads();
 		const embed = new MessageEmbed()
 			.setColor('#E74C3C')
 			.setTitle(`Server information for ${interaction.guild.name}`)
@@ -19,13 +29,17 @@ module.exports = {
 			.addFields(
 				{ name: 'Server owner', value: `${GuildOwner}`, inline: true },
 				{ name: 'Server language', value: `${interaction.guild.preferredLocale}`, inline: true },
-				{ name: 'Channels', value: `Channels ${Channels}\nThreads: ${Threads}`, inline: true },
+				{ name: 'Channels', value: `Text: \`${Channels}\`
+											Threads: \`${Public_Threads + Private_Threads + News_Threads}\`
+											Voice: \`${Voice}\``, inline: true },
 				{ name: '\u200B', value: '\u200B' },
-				{ name: 'Inline field title', value: 'Some value here', inline: true },
+				{ name: 'Members', value: `Users: \`${Users}\`
+											Bots: \`${Bots}\`
+											Members: \`${Members}\``, inline: true },
 				{ name: 'Inline field title', value: 'Some value here', inline: true },
 			)
 			.addField('Inline field title', 'Some value here', true)
-			.setImage('https://i.imgur.com/AfFp7pu.png')
+			// .setImage('https://i.imgur.com/AfFp7pu.png')
 			// .setTimestamp()
 			.setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
 		await interaction.reply({ embeds: [embed] });
